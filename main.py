70933c53e52c0550d1b3d4bed72977f00a2b28d8
diego244thales-maker <diego244thales@gmail.com>
2026-09-03 00:40:48 -0300

    Add product registration functionality

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("ola,mundo)
+produtos = []
+
+nome = input("Digite o nome do produto: ")
+preco = float(input("Digite o preço do produto: "))
+quantidade = int(input("Digite a quantidade: "))
+
+produto = {
+    "nome": nome,
+    "preco": preco,
+    "quantidade": quantidade
+}
+
+produtos.append(produto)
+
+print("\nProduto cadastrado!")
+print(produtos)
